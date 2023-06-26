b99d987f78b5f22b59a13468f981b5d116a46493
‪Ori David Haknin <133655211+Haknin@users.noreply.github.com>
2023-06-26 21:05:42 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,10 @@ pgrep flask
 
 if [[ "$?" == "0" ]]; then
   pkill flask
+  sleep 7
+  cd /home/ec2-user/crypto-site
+  flask run --host=0.0.0.0 &&
+  
 fi
 cd /home/ec2-user/crypto-site
 flask run --host=0.0.0.0 &&
